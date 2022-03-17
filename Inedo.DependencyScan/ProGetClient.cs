0b80b2fa0107a198d778d63212dc7098cf18729e
Greg Divis <gdivis@inedo.com>
2022-03-16 21:59:57 -0400

    Add support for sending multiple dependents in one request for ProGet 6.0.11 and later. #6

--- Inedo.DependencyScan/ProGetClient.cs
+++ Inedo.DependencyScan/ProGetClient.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 #if !NET452
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -15,6 +17,8 @@ namespace Inedo.DependencyScan
 {
     internal sealed class ProGetClient
     {
+        private static readonly Version MinBatchServerVersion = new(6, 0, 11);
+
         public ProGetClient(string baseUrl)
         {
             this.BaseUrl = baseUrl.TrimEnd('/');
@@ -22,7 +26,27 @@ namespace Inedo.DependencyScan
 
         public string BaseUrl { get; }
 
-        public async Task RecordPackageDependencyAsync(DependencyPackage package, string feed, PackageConsumer consumer, string apiKey, string comments = null)
+        public Task RecordPackageDependencyAsync(DependencyPackage package, string feed, PackageConsumer consumer, string apiKey, string comments = null) => this.RecordPackageDependencyInternalAsync(package, feed, consumer, apiKey, comments);
+
+        public async Task RecordPackageDependenciesAsync(IEnumerable<DependencyPackage> packages, string feed, PackageConsumer consumer, string apiKey, string comments = null)
+        {
+            Version serverVersion = null;
+
+            var remainingPackages = new List<DependencyPackage>();
+
+            foreach (var package in packages)
+            {
+                if (serverVersion == null || serverVersion < MinBatchServerVersion)
+                    serverVersion = await this.RecordPackageDependencyInternalAsync(package, feed, consumer, apiKey, comments).ConfigureAwait(false);
+                else
+                    remainingPackages.Add(package);
+            }
+
+            if (remainingPackages.Count > 0)
+                await this.RecordPackageDependenciesInternalAsync(remainingPackages, feed, consumer, apiKey, comments).ConfigureAwait(false);
+        }
+
+        private async Task<Version> RecordPackageDependencyInternalAsync(DependencyPackage package, string feed, PackageConsumer consumer, string apiKey, string comments)
         {
             var request = WebRequest.CreateHttp(this.BaseUrl + "/api/dependencies/dependents");
             request.Method = "POST";
@@ -46,6 +70,47 @@ namespace Inedo.DependencyScan
                         NullValueHandling = NullValueHandling.Ignore
                     }.Serialize(writer, new DependentPackage(package, feed, consumer, comments));
                 }
+#endif
+            }
+
+            try
+            {
+                using var response = await request.GetResponseAsync().ConfigureAwait(false);
+                if (Version.TryParse(response.Headers["X-ProGet-Version"], out var version))
+                    return version;
+                else
+                    return null;
+            }
+            catch (WebException ex) when (ex.Response is HttpWebResponse response)
+            {
+                using var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                var message = new char[8192];
+                int length = reader.ReadBlock(message, 0, message.Length);
+                if (length > 0)
+                    throw new InvalidOperationException($"Server responded with {(int)response.StatusCode} {response.StatusDescription}: {new string(message, 0, length)}") { Data = { ["message"] = true } };
+                else
+                    throw new InvalidOperationException($"Server responded with {(int)response.StatusCode} {response.StatusDescription}") { Data = { ["message"] = true } };
+            }
+        }
+
+        private async Task RecordPackageDependenciesInternalAsync(List<DependencyPackage> packages, string feed, PackageConsumer consumer, string apiKey, string comments)
+        {
+            var request = WebRequest.CreateHttp(this.BaseUrl + "/api/dependencies/dependents");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            using (var requestStream = await request.GetRequestStreamAsync().ConfigureAwait(false))
+            {
+#if !NET452
+                using var writer = new Utf8JsonWriter(requestStream);
+                JsonSerializer.Serialize(
+                    writer,
+                    packages.Select(p => new DependentPackage(p, feed, consumer, comments)),
+                    new JsonSerializerOptions { IgnoreNullValues = true }
+                );
+#else
+                using var writer = new StreamWriter(requestStream, Encoding.UTF8);
+                new JsonSerializer { NullValueHandling = NullValueHandling.Ignore }.Serialize(writer, packages.Select(p => new DependentPackage(p, feed, consumer, comments)));
 #endif
             }
 
